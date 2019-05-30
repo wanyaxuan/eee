a434b79966273a579ea5682b41a2ad21ec6c4ba8
wanyaxuan <2547838085@qq.com>
2019-05-30 08:47:57 +0800

    22

---
+++ 30.php
@@ -0,0 +1,14 @@
+<?php
+$n=10;
+function NumberOf1($n){
+ for($n=0;$n<=1;$n++){
+     if($n==0){
+         return 0;
+     }else if($n==1){
+         return 1;
+     }else{
+         return 0;
+     }
+ }
+}
+?>
