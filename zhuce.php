56d3026d2aaa4b24388f93d26b62fbee712954e5
wanyaxuan <2547838085@qq.com>
2019-06-13 08:42:57 +0800

    zhuce

---
+++ zhuce.php
@@ -0,0 +1,7 @@
+<from>
+    <table>
+        用户名：<input type="text" name="user_name"/>
+        密码：<input type="text" name="user_pwd"/>
+        <input type="submit" value="注册"/>
+    </table>
+</from>
